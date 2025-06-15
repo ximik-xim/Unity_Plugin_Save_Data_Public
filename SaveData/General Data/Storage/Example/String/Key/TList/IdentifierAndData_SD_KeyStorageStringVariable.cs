d4281407b62952f869fc2bb7b27ad4437fe455e8
NO-243PCBER03I9\ENP <ximik-xim@mail.ru>
2025-06-15 13:11:10 +0300

    - Добавление поддержки TList(0.6.0) - Добавление бэкапов

--- SaveData/General Data/Storage/Example/String/Key/TList/IdentifierAndData_SD_KeyStorageStringVariable.cs
+++ SaveData/General Data/Storage/Example/String/Key/TList/IdentifierAndData_SD_KeyStorageStringVariable.cs
@@ -12,4 +12,16 @@ public class IdentifierAndData_SD_KeyStorageStringVariable : AbsIdentifierAndDat
  {
   return _dataKey;
  }
+ 
+#if UNITY_EDITOR
+ public override string GetJsonSaveData()
+ {
+  return JsonUtility.ToJson(_dataKey);
+ }
+
+ public override void SetJsonData(string json)
+ {
+  _dataKey = JsonUtility.FromJson<SD_KeyStorageStringVariable>(json);
+ }
+#endif
 }
